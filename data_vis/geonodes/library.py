7ef2d7d0f3349e7024cc9a2f4c93874e73de1272
Zdenek Dolezal <Griperis@outlook.cz>
2024-02-11 21:34:00 +0100

    Link nodegroups by default

--- data_vis/geonodes/library.py
+++ data_vis/geonodes/library.py
@@ -6,23 +6,23 @@ GEONODES_BLENDS_PATH = os.path.abspath(
     os.path.join(os.path.dirname(__file__), "../blends/chart_geonodes.blend"))
 
 
-def _load_nodegroup(name: str) -> bpy.types.NodeGroup:
+def _load_nodegroup(name: str, link: bool = True) -> bpy.types.NodeGroup:
     if not os.path.isfile(GEONODES_BLENDS_PATH):
         raise FileNotFoundError(f"Geometry nodes library couldn't be found at {GEONODES_BLENDS_PATH}")
-    with bpy.data.libraries.load(GEONODES_BLENDS_PATH, link=False) as (data_from, data_to):
+    with bpy.data.libraries.load(GEONODES_BLENDS_PATH, link=link) as (data_from, data_to):
         assert name in data_from.node_groups
         data_to.node_groups = [name]
 
     return data_to.node_groups[0]
 
 
-def load_chart(name: str):
-    return _load_nodegroup(name)
+def load_chart(name: str, link: bool = True):
+    return _load_nodegroup(name, link)
 
 
-def load_axis() -> bpy.types.NodeGroup:
-    return _load_nodegroup("DV_NumericAxis")
+def load_axis(link: bool = True) -> bpy.types.NodeGroup:
+    return _load_nodegroup("DV_NumericAxis", link)
 
 
-def load_above_data_labels() -> bpy.types.NodeGroup:
-    return _load_nodegroup("DV_DataLabels")
+def load_above_data_labels(link: bool = True) -> bpy.types.NodeGroup:
+    return _load_nodegroup("DV_DataLabels", link)
